b36b6f5127ffcbcb036b43adce5960c0f6249de2
Dmitry Kulevich <dmitry.kulevich@capsilon.com>
2019-03-04 23:57:13 +0300

    feat: add implementation of getLoveTrianglesCount function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+var loveTrianglesCount = 0;
+  for(var i = 0; i < preferences.length; i++) {
+    if (preferences[i] > 0 && preferences[i] != i + 1 && preferences[preferences[preferences[i] - 1] - 1] == i + 1) {
+      loveTrianglesCount++;
+      preferences[i] = 0;
+    }
+  }
+  return loveTrianglesCount;
 };
